b6d4cc67fd2346adf03ce12fa8744c2898fa8faa
Carlos A Angulo Sermeño <carlos@c3consensus.com>
2024-03-22 20:34:25 -0600

    Add project files.

---
+++ ToDoApi/Controllers/ToDoItemsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoApi.Models;
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoItemsController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoItemsController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ToDoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoItemDTO>>> GetToDoItems()
+        {
+            //return await _context.ToDoItems.ToListAsync();
+            return await _context.ToDoItems
+                .Select(x => ItemToDTO(x)).ToListAsync();
+        }
+
+        private static ToDoItemDTO ItemToDTO(ToDoItem toDoItem) =>
+            new ToDoItemDTO
+            {
+                ToDoItemId = toDoItem.ToDoItemId,
+                Name = toDoItem.Name,
+                IsComplete = toDoItem.IsComplete
+            };
+        
+
+        // GET: api/ToDoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoItemDTO>> GetToDoItem(long id)
+        {
+            var toDoItem = await _context.ToDoItems.FindAsync(id);
+
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            return ItemToDTO(toDoItem);
+        }
+
+        // PUT: api/ToDoItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDoItem(long id, ToDoItemDTO toDoDTO)
+        {
+            if (id != toDoDTO.ToDoItemId)
+            {
+                return BadRequest();
+            }
+
+            var toDoItem = await _context.ToDoItems.FindAsync(id);
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            
+
+            toDoItem.Name = toDoDTO.Name;
+            toDoItem.IsComplete = toDoDTO.IsComplete;
+
+            _context.Entry(toDoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) 
+            {
+                if (!ToDoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ToDoItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ToDoItemDTO>> PostToDoItem(ToDoItemDTO toDoDTO)
+        {
+            ToDoItem toDoItem = new ToDoItem()
+            {
+                IsComplete = toDoDTO.IsComplete,
+                Name = toDoDTO.Name
+            };
+
+            _context.ToDoItems.Add(toDoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetToDoItem", new { id = toDoItem.ToDoItemId }, ItemToDTO(toDoItem));
+        }
+
+        // DELETE: api/ToDoItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDoItem(long id)
+        {
+            var toDoItem = await _context.ToDoItems.FindAsync(id);
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDoItems.Remove(toDoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ToDoItemExists(long id)
+        {
+            return _context.ToDoItems.Any(e => e.ToDoItemId == id);
+        }
+    }
+}
---
+++ ToDoApi/Models/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApi.Models
+{
+    public class ToDoItem
+    {
+        public long ToDoItemId { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
+
+    }
+}
